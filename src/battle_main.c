e8479fd6a9374d2a2373abb67bf03bebf42999ad
fakuzatsu <abyerswilson@gmail.com>
2023-09-29 21:05:37 +0100

    changed incorrect definition of cherrim

--- src/battle_main.c
+++ src/battle_main.c
@@ -4616,7 +4616,7 @@ u32 GetBattlerTotalSpeedStatArgs(u32 battler, u32 ability, u32 holdEffect)
             speed *= 2;
         else if (ability == ABILITY_SLUSH_RUSH  && (gBattleWeather & (B_WEATHER_HAIL | B_WEATHER_SNOW)))
             speed *= 2;
-        else if (ability == ABILITY_FLOWER_GIFT && species == CHERRIM_SUNSHINE && holdEffect != HOLD_EFFECT_UTILITY_UMBRELLA && gBattleWeather & B_WEATHER_SUN)
+        else if (ability == ABILITY_FLOWER_GIFT && species == SPECIES_CHERRIM_SUNSHINE && holdEffect != HOLD_EFFECT_UTILITY_UMBRELLA && gBattleWeather & B_WEATHER_SUN)
             speed *= 1.5;
     }
 
